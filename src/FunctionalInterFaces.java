3d513caa6cfecbd3baa1694cb376ece5876379db
Shivam Kumar <shivamkumargupta07@gmail.com>
2021-12-11 17:06:15 +0530

    Adding PredicateExample

--- src/FunctionalInterFaces.java
+++
@@ -1,9 +0,0 @@
-import java.util.function.Predicate;
-
-public class FunctionalInterFaces {
-	public static void main(String[] args) {
-		
-		Predicate<Integer> StringLength = (s)->s < 5;
-		System.out.println(StringLength.test(6));
-	}
-}
---
+++ src/functionalInterface/PredicateExample.java
@@ -0,0 +1,19 @@
+package functionalInterface;
+
+import java.util.function.Predicate;
+
+public class PredicateExample {
+	public void testingPredicateMethod(String temp){
+		
+	}
+	public static void main(String[] args) {
+		System.out.println("PredicateExample...");
+		Predicate <String> test = (temp)->{
+			if("pass".equals(temp))
+				return true;
+			return false;
+		};
+		System.out.println(test.test("Check"));		//Test method contained by Predicate Interface
+		System.out.println(test.test("pass"));		//It will Return boolean only
+	}
+}
